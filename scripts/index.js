87f4ac7393df7e995950385c5c974bed93c5da55
TonniPaul <ariyoadeoyepaul@gmail.com>
2023-03-23 12:37:57 +0100

    Completed technplogy Page

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,15 @@
+// Toggle Menu Function
+// Variable Declaration
 const menuIcon = document.querySelector(".mb-menu");
 const navItems = document.querySelector(".nav-items");
+const closeIcon = "/assets/shared/icon-close.svg";
+const hamburgerMenu = "/assets/shared/icon-hamburger.svg";
 
 menuIcon.addEventListener("click", () => {
   navItems.classList.toggle("show");
+
+  // Switch Menu Icons if Nav is Opened
+  if (navItems.classList.contains("show")) {
+    menuIcon.setAttribute("src", closeIcon);
+  } else menuIcon.setAttribute("src", hamburgerMenu);
 });
